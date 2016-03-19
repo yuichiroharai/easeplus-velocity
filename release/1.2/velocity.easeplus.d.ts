67440f2f220930b91d4c7eb26b279182579a384c
Yuichiroh Arai <yuichiroh.arai@gmail.com>
2016-03-19 18:25:08 +0900

    TypeScript用のd.tsファイルを追加。

---
+++ release/1.2/velocity.easeplus.d.ts
@@ -0,0 +1,30 @@
+// Type definitions for Ease+ v1.2
+// Project: https://github.com/yuichiroharai/easeplus-velocity/
+// Definitions by: Yuichiroh Arai <https://github.com/yuichiroharai/>
+// Definitions: https://github.com/yuichiroharai/easeplus-velocity/
+
+interface JQueryStatic {
+    easeplus: jquery.easeplus.EasePlusStatic;
+}
+
+declare namespace jquery.easeplus {
+
+    interface EasePlusStatic {
+        addBackIn(name:string, amount:number);
+        addBackOut(name:string, amount:number);
+        addBackInOut(name:string, amount:number);
+
+        addElasticIn(name:string, amplitude:number, period:number);
+        addElasticOut(name:string, amplitude:number, period:number);
+        addElasticInOut(name:string, amplitude:number, period:number);
+
+        addElasticIn(name:string, amplitude:number, period:number);
+        addElasticOut(name:string, amplitude:number, period:number);
+        addElasticInOut(name:string, amplitude:number, period:number);
+
+        addBlend(name:string, ease1:string, ease2:string, easeMix:string);
+
+        remove(name:string);
+    }
+
+}
